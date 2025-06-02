432266c3c2924288bf055d79de8eec6a3e0b1472
juliheit <heitjulieta@gmail.com>
2025-06-02 18:10:02 -0300

    saludo minitp

---
+++ script.js
@@ -0,0 +1,7 @@
+//Mensaje para el usuario para contactarnos
+alert(
+  `Hola ${nombre}, bienvenido a nuestra sección de contacto. Rellena el formulario para poder ayudarte en lo que necesites. Muchas gracias!`
+);
+console.log(
+  `Hola ${nombre}, bienvenido a nuestra sección de contacto. Rellena el formulario para poder ayudarte en lo que necesites. Muchas gracias!`
+);
